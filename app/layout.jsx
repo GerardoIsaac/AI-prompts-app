a74e43215b8344a91e430d83a9f98ba73d0a0e15
Gerardo Castaneda <zl1fmercury@gmail.com>
2023-10-18 17:38:58 -0600

    create handlers for requests

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,23 +7,21 @@ export const metadata = {
   description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
 
-          <div className="app">
-            <Nav />
-            {children}
-          </div>
-        </Provider>
-      </body>
-    </html>
-  );
-};
+        <div className="app">
+          <Nav />
+          {children}
+        </div>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
