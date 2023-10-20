90dc6f4ff71293d267c90e03938102ec372307a1
Gerardo Castaneda <zl1fmercury@gmail.com>
2023-10-20 12:58:37 -0600

    create prompt page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -10,7 +10,27 @@ const CreatePrompt = () => {
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: {
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: prompt.tag,
+        },
+      });
+
+      if (response.ok) router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
