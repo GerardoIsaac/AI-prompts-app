513b715174858b9a44a2ae0db5f7e76dcc469d6a
Gerardo Castaneda <zl1fmercury@gmail.com>
2023-10-22 17:22:20 -0600

    add profile page for other users

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import { useSearchParams } from "next/navigation";
+
+import Profile from "@components/Profile";
+import { useEffect, useState } from "react";
+
+const page = ({ params }) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
+
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params.id}/posts`);
+      const data = await response.json();
+
+      setUserPosts(data);
+    };
+
+    if (params?.id) fetchPosts();
+  }, [params.id]);
+
   return (
-    <div>page</div>
-  )
-}
+    <Profile
+      name={userName}
+      desc={`Welcome to ${userName}'s profile page. Explore ${userName}'s
+        exceptional prompts inspired by the power of their imagination`}
+      data={userPosts}
+    />
+  );
+};
 
-export default page
+export default page;
