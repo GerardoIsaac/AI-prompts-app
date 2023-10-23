bf0a813348a72670166a307640cdbee97ebe9738
Gerardo Castaneda <zl1fmercury@gmail.com>
2023-10-23 12:27:23 -0600

    hash update

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -67,19 +67,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
-        className="font-inter text-sm cursor-pointer"
+        className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {
-          post.tag.split(' ').map((tag, i) => {
-            const cleanedTag =  tag.trim()
-
-            const tagWithoutHash = cleanedTag.replace('#', '')
-            return (
-              <span key={i} className="blue_gradient">#{tagWithoutHash}{' '}</span>
-            )
-          })
-        }
+        {post.tag.includes("#") ? post.tag : `#${post.tag}`}
       </p>
 
       {session?.user.id === post.creator._id && pathName === "/profile" && (
